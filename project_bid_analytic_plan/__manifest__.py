49a4e7a86812b7ee0b1a3341818d6244e86c7f68
aheficent <ahenriquez@eficent.com>
2017-09-06 11:32:41 +0200

    [MIG][10.0]Project_bid_analytic_plan

--- project_bid_analytic_plan/__manifest__.py
+++ project_bid_analytic_plan/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L.
+# © 2015-17 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Project Bid - Analytic Plan integration",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent",
     "website": "www.eficent.com",
     "category": "Generic Modules/Projects & Services",
